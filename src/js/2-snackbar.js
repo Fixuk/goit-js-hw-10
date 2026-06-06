a32df52a300b1d1796639ec8391cff2c80e5251c
Kirill <grinders92@gmail.com>
2026-06-06 23:21:30 +0300

    add homework files

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      Math.random() > 0.5 ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+
+  promise
+    .then(ms => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${ms}ms`,
+        messageColor: '#fff',
+        backgroundColor: '#59a10d',
+        position: 'topRight',
+        timeout: 5000,
+      });
+    })
+    .catch(ms => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${ms}ms`,
+        messageColor: '#fff',
+        backgroundColor: '#ee3f3f',
+        position: 'topRight',
+        timeout: 5000,
+      });
+    });
+});
